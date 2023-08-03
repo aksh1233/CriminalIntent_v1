284ad09ac6bc29e85d4f24970e5d753d06bb59e4
akshaydubey <divine.akshay@gmail.com>
2023-08-03 22:29:13 +0530

    ArgumentBundle_and_getting_IntentExtra

--- app/src/main/java/com/example/criminalintent_v1/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent_v1/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.criminalintent_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -111,8 +112,9 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onClick(View view)
         {
-            Toast.makeText(getActivity(),mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            startActivity(intent);
+
         }
 
         public void bind(Crime crime)
