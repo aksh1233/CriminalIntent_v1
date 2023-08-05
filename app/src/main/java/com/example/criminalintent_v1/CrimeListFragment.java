e9fd7c42ff51201fcb2ba5e6166cbcdf666ec1c4
akshaydubey <divine.akshay@gmail.com>
2023-08-05 11:15:48 +0530

    Add_ViewPager

--- app/src/main/java/com/example/criminalintent_v1/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent_v1/CrimeListFragment.java
@@ -126,7 +126,7 @@ public class CrimeListFragment extends Fragment {
         public void onClick(View view)
         {
 
-            Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(),mCrime.getId());
             startActivity(intent);
 
         }
